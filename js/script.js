4ba36deeba301c2e37677626fb8c0a18c51abd92
anandhukrishna1998 <63602359+anandhukrishna1998@users.noreply.github.com>
2024-01-27 20:53:01 +0100

    darkmode testing

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Check if user has a preferred color scheme
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    document.body.classList.add('dark-mode');
+}
+
+// Toggle dark mode on button click
+document.getElementById('toggleDarkMode').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
